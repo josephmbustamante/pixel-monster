d096d61fbc132f2df2183a1cb92124dfb61b918e
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-05-11 21:08:10 -0400

    Pull platforms out into their own module

---
+++ src/platforms/index.ts
@@ -0,0 +1,21 @@
+
+const PLATFORM_WIDTH = 50;
+const PLATFORM_HEIGHT = 50;
+
+const PLATFORM_COLOR = 0xff0000;
+
+export interface Platform {
+  gameObject: Phaser.GameObjects.Rectangle;
+  physicsBody: Phaser.Physics.Arcade.StaticBody;
+}
+
+export const createPlatform = (scene: Phaser.Scene, x: number, y: number) => {
+  const platform = scene.add.rectangle(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT, PLATFORM_COLOR)
+    .setOrigin(0, 0).setInteractive();
+  const staticPhysicsBody = scene.physics.add.existing(platform, true);
+
+  return {
+    gameObject: platform,
+    physicsBody: staticPhysicsBody,
+  };
+};
