4a9e1cd59ec5cff8881dee3f39d08ec05bbf64b0
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-05-12 19:06:28 -0400

    Give players and platforms default colors

--- src/platforms/index.ts
+++ src/platforms/index.ts
@@ -4,18 +4,23 @@ const PLATFORM_WIDTH = 50;
 const PLATFORM_HEIGHT = 50;
 
 export interface Platform {
+  color: Shared.Color;
   gameObject: Phaser.GameObjects.Rectangle;
   physicsBody: Phaser.Physics.Arcade.StaticBody;
 }
 
 export const createPlatform = (scene: Phaser.Scene, x: number, y: number) => {
-  const platform = scene.add.rectangle(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT, Shared.USED_TILE_COLOR_HEX)
+  const color = { r: 128, g: 128, b: 128 };
+  const colorHex = Shared.convertFullColorToHex(color);
+
+  const platform = scene.add.rectangle(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT, colorHex)
     .setOrigin(0, 0).setInteractive();
 
   scene.physics.add.existing(platform, true);
   const staticPhysicsBody = platform.body as Phaser.Physics.Arcade.StaticBody;
 
   return {
+    color,
     gameObject: platform,
     physicsBody: staticPhysicsBody,
   };
--- src/player/index.ts
+++ src/player/index.ts
@@ -4,18 +4,23 @@ const PLAYER_WIDTH = 50;
 const PLAYER_HEIGHT = 50;
 
 export interface Player {
+  color: Shared.Color;
   gameObject: Phaser.GameObjects.Rectangle;
   physicsBody: Phaser.Physics.Arcade.Body;
 }
 
 export const createPlayer = (scene: Phaser.Scene, x: number, y: number): Player => {
-  const playerGameObject = scene.add.rectangle(x, y, PLAYER_WIDTH, PLAYER_HEIGHT, Shared.PLAYER_STARTING_COLOR_HEX)
+  const startingColor = { r: 0, g: 0, b: 0 };
+  const startingColorHex = Shared.convertFullColorToHex(startingColor);
+
+  const playerGameObject = scene.add.rectangle(x, y, PLAYER_WIDTH, PLAYER_HEIGHT, startingColorHex)
     .setOrigin(0, 0).setInteractive();
 
   scene.physics.add.existing(playerGameObject, false);
   const playerPhysicsBody = playerGameObject.body as Phaser.Physics.Arcade.Body;
 
   return {
+    color: startingColor,
     gameObject: playerGameObject,
     physicsBody: playerPhysicsBody,
   };
